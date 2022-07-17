a829391b918598e6254563e48aa1af8eed8007df
unknown <ankushvaish77@gmail.com>
2022-07-18 02:21:47 +0530

    Unsplash url correction

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.unsplash.comkkk',
+    baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization: 'Client-ID CqVBeFXLsxzg1F_8sFMOWBuPFhUwgtXi-m_22CrHkrc',
     },
